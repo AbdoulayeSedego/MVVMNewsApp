f36bcf4f87952bc0a48a7bac830f1a6d342c862f
Abdoulaye Sedego <abdoulaye.sedego95@gmail.com>
2023-01-22 07:56:29 -0500

    set up navigation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.ui.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.ui.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}
