e67fb5d613208b768ad007b2f7604cce3fd43a6a
Abdoulaye Sedego <abdoulaye.sedego95@gmail.com>
2023-01-27 03:35:56 -0500

    Setup Retrofite

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "1cf11e6aea7c47e591ed1ec2ca9b668b"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
